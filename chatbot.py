e2b55b94a6e27c031ced87902c5ec73ddfa167a5
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-08-25 04:29:56 +0100

    added notebook that contains regex code to scrape information

--- chatbot.py
+++ chatbot.py
@@ -7,8 +7,15 @@ order #56475647483
 order#574748494904
 """
 
-pattern = re.compile(r"(order)\s\#[0-9]+")
+pattern = re.compile(r"(order)\s\#\d+")
 matches = pattern.finditer(content)
 
 for match in matches:
     print(match.group())
+
+chat1 = "(123)-567-8912"
+chat2 = "1234558494940"
+
+pattern1 = r"\(?\d+\)?-?\d{3}?-?\d{4}?"
+matches = re.findall(pattern1, chat1)
+print(matches)
