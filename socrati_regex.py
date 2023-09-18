13b4550b3fcf27af6243a00f9c20ad58b49e21a3
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-09-18 11:40:12 +0100

    catching cases with one or more white spaces

--- socrati_regex.py
+++ socrati_regex.py
@@ -1,14 +1,14 @@
 import re
 
-names = ['Finn Bindeballe',
+names = ['Finn   Bindeballe',
         'Geir Anders Berge',
         'HappyCodingRobot',
-        'Ron Cromberge',
+        'Ron       Cromberge',
         'Sohil'
         ]
 
 # Find people with first and last name only
-regex = r'^\w+ \w+$'
+regex = r'^\w+\s+\w+$'
 
 for name in names:
     result = re.search(regex, name)
